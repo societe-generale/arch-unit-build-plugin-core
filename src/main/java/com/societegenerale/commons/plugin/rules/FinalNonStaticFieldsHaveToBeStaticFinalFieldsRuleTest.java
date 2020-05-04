10aa18650959f302d83f26dc9f0875094956518b
FanJups <jupsfan@gmail.com>
2020-05-04 15:26:09 +0200

    Adding FinalNonStaticFieldsHaveToBeStaticFinalFieldsRuleTest

---
+++ src/main/java/com/societegenerale/commons/plugin/rules/FinalNonStaticFieldsHaveToBeStaticFinalFieldsRuleTest.java
@@ -0,0 +1,36 @@
+package com.societegenerale.commons.plugin.rules;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.fields;
+
+import java.util.Collection;
+
+import com.societegenerale.commons.plugin.service.ScopePathProvider;
+import com.societegenerale.commons.plugin.utils.ArchUtils;
+
+/**
+ * 
+ * Final Non Static Fields have to be Static Final Fields
+ * 
+ * @see <a href=
+ *      "https://stackoverflow.com/questions/35095483/why-should-data-fields-be-static-and-final">why-should-data-fields-be-static-and-final</a>
+ * 
+ * @see <a href=
+ *      "https://rules.sonarsource.com/java/tag/convention/RSPEC-1170">Public
+ *      constants and fields initialized at declaration should be "static final"
+ *      rather than merely "final"</a>
+ */
+
+public class FinalNonStaticFieldsHaveToBeStaticFinalFieldsRuleTest implements ArchRuleTest {
+
+	public static final String FINAL_NON_STATIC_FIELDS_VIOLATION_MESSAGE = "you are duplicating its value for every instance of the class, uselessly increasing the amount of memory required to execute the application.";
+
+	@Override
+	public void execute(String path, ScopePathProvider scopePathProvider, Collection<String> excludedPaths) {
+
+		fields().that().areFinal().and().areNotStatic().should().beStatic().andShould().beFinal()
+				.because(FINAL_NON_STATIC_FIELDS_VIOLATION_MESSAGE).check(ArchUtils.importAllClassesInPackage(path,
+						scopePathProvider.getMainClassesPath(), excludedPaths));
+
+	}
+
+}
---
+++ src/test/java/com/societegenerale/aut/test/ClassWithStaticFinalFields.java
@@ -0,0 +1,7 @@
+package com.societegenerale.aut.test;
+
+public class ClassWithStaticFinalFields {
+
+	public static final int NUMBER_OF_DEVELOPERS = 10;
+
+}
