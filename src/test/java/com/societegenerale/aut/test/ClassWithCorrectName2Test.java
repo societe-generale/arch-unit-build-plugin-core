2998c1d29c97a644e6af2e92331cfc697e1d7554
Fanon Jupkwo <jupsfan@gmail.com>
2020-06-03 21:10:08 +0100

    Test Naming Rule (#22)

---
+++ src/test/java/com/societegenerale/aut/test/ClassWithCorrectName2Test.java
@@ -0,0 +1,15 @@
+package com.societegenerale.aut.test;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Nested;
+
+public class ClassWithCorrectName2Test {
+
+	@Nested
+	class PositiveCase {
+		@Test
+		public void check() {
+		}
+	}
+
+}
