e01d3dd8cdb8218e1771cd0ad6fb1508f3c7ae0b
Fanon Jupkwo <jupsfan@gmail.com>
2020-07-05 10:04:34 +0100

    Refactoring NoPowerMockRuleTest (#33)

--- src/test/java/com/societegenerale/commons/plugin/rules/NoPowerMockRuleTestTest.java
+++ src/test/java/com/societegenerale/commons/plugin/rules/NoPowerMockRuleTestTest.java
@@ -1,44 +1,54 @@
 package com.societegenerale.commons.plugin.rules;
 
-import com.societegenerale.aut.test.TestClassWithOutJunitAsserts;
-import com.societegenerale.aut.test.TestClassWithPowerMock;
-import com.tngtech.archunit.core.domain.JavaClasses;
-import com.tngtech.archunit.core.importer.ClassFileImporter;
+import static com.societegenerale.commons.plugin.rules.NoPowerMockRuleTest.POWER_MOCK_VIOLATION_MESSAGE;
+import static java.util.Collections.emptySet;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.catchThrowable;
+
+import org.junit.Before;
 import org.junit.Test;
 
-import static com.societegenerale.commons.plugin.rules.NoPowerMockRuleTest.POWER_MOCK_VIOLATION_MESSAGE;
-import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
-import static org.assertj.core.api.Assertions.*;
+import com.societegenerale.aut.test.TestClassWithPowerMock;
+import com.societegenerale.commons.plugin.SilentLog;
+import com.societegenerale.commons.plugin.service.DefaultScopePathProvider;
+import com.societegenerale.commons.plugin.utils.ArchUtils;
 
 public class NoPowerMockRuleTestTest {
 
-    private JavaClasses testClassWithPowerMock = new ClassFileImporter().importClasses(TestClassWithPowerMock.class);
-    private JavaClasses testClassWithoutPowerMock = new ClassFileImporter().importClasses(TestClassWithOutJunitAsserts.class);
+	// testClassWithoutPowerMock
+	private String pathTestClassWithOutJunitAsserts = "./target/aut-target/test-classes/com/societegenerale/aut/test/TestClassWithOutJunitAsserts.class";
 
+	private String pathTestClassWithPowerMock = "./target/aut-target/test-classes/com/societegenerale/aut/test/TestClassWithPowerMock.class";
 
-    @Test
-    public void shouldNotThrowAnyViolation(){
-        assertThatCode(
-                () -> classes().should(NoPowerMockRuleTest.notUsePowerMock()).check(testClassWithoutPowerMock))
-                .doesNotThrowAnyException();
-    }
+	@Before
+	public void setup() {
+		// in the normal lifecycle, ArchUtils is instantiated, which enables a static
+		// field there to be initialized
+		ArchUtils archUtils = new ArchUtils(new SilentLog());
+	}
 
+	@Test
+	public void shouldNotThrowAnyViolation() {
+		assertThatCode(() -> new NoPowerMockRuleTest().execute(pathTestClassWithOutJunitAsserts,
+				new DefaultScopePathProvider(), emptySet())).doesNotThrowAnyException();
 
-    @Test
-    public void shouldThrowViolations(){
+	}
 
-        Throwable validationExceptionThrown = catchThrowable(() -> {
+	@Test
+	public void shouldThrowViolations() {
 
-            classes().should(NoPowerMockRuleTest.notUsePowerMock()).check(testClassWithPowerMock);
+		Throwable validationExceptionThrown = catchThrowable(() -> {
 
-        });
+			new NoPowerMockRuleTest().execute(pathTestClassWithPowerMock, new DefaultScopePathProvider(), emptySet());
 
-        assertThat(validationExceptionThrown).isInstanceOf(AssertionError.class)
-                .hasMessageStartingWith("Architecture Violation")
-                .hasMessageContaining("was violated (1 times)")
-                .hasMessageContaining(TestClassWithPowerMock.class.getName())
-                .hasMessageContaining(POWER_MOCK_VIOLATION_MESSAGE);
+		});
 
-    }
+		assertThat(validationExceptionThrown).isInstanceOf(AssertionError.class)
+				.hasMessageStartingWith("Architecture Violation").hasMessageContaining("was violated (1 times)")
+				.hasMessageContaining(TestClassWithPowerMock.class.getName())
+				.hasMessageContaining(POWER_MOCK_VIOLATION_MESSAGE);
+
+	}
 
 }
