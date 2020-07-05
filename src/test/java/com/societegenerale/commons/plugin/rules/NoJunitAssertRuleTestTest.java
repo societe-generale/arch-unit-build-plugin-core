764fcf2bb7ab043dd74bb5e78a89310413317cec
Fanon Jupkwo <jupsfan@gmail.com>
2020-07-05 10:06:51 +0100

    Refactoring No Junit Assert Rule Test (#32)

--- src/test/java/com/societegenerale/commons/plugin/rules/NoJunitAssertRuleTestTest.java
+++ src/test/java/com/societegenerale/commons/plugin/rules/NoJunitAssertRuleTestTest.java
@@ -1,66 +1,80 @@
 package com.societegenerale.commons.plugin.rules;
 
-import com.societegenerale.aut.test.TestClassWithJunit4Asserts;
-import com.societegenerale.aut.test.TestClassWithJunit5Asserts;
-import com.societegenerale.aut.test.TestClassWithOutJunitAsserts;
-import com.tngtech.archunit.core.domain.JavaClasses;
-import com.tngtech.archunit.core.importer.ClassFileImporter;
+import static com.societegenerale.commons.plugin.rules.NoJunitAssertRuleTest.NO_JUNIT_ASSERT_DESCRIPTION;
+import static java.util.Collections.emptySet;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.catchThrowable;
+
+import org.junit.Before;
 import org.junit.Test;
 
-import static com.societegenerale.commons.plugin.rules.NoJunitAssertRuleTest.NO_JUNIT_ASSERT_DESCRIPTION;
-import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
-import static org.assertj.core.api.Assertions.*;
+import com.societegenerale.aut.test.TestClassWithJunit4Asserts;
+import com.societegenerale.aut.test.TestClassWithJunit5Asserts;
+import com.societegenerale.commons.plugin.SilentLog;
+import com.societegenerale.commons.plugin.service.DefaultScopePathProvider;
+import com.societegenerale.commons.plugin.utils.ArchUtils;
 
 public class NoJunitAssertRuleTestTest {
 
-    private JavaClasses testClassWithJunit4Asserts = new ClassFileImporter().importClasses(TestClassWithJunit4Asserts.class);
-    private JavaClasses testClassWithJunit5Asserts = new ClassFileImporter().importClasses(TestClassWithJunit5Asserts.class);
-    private JavaClasses  tesClassesWithoutJunitAsserts = new ClassFileImporter().importClasses(TestClassWithOutJunitAsserts.class);
+	private String pathTestClassWithJunit4Asserts = "./target/aut-target/test-classes/com/societegenerale/aut/test/TestClassWithJunit4Asserts.class";
 
-    @Test
-    public void shouldNotThrowAnyViolation(){
-        assertThatCode(
-                () -> classes().should(NoJunitAssertRuleTest.notUseJunitAssertRule()).check(tesClassesWithoutJunitAsserts))
-                .doesNotThrowAnyException();
-    }
+	private String pathTestClassWithJunit5Asserts = "./target/aut-target/test-classes/com/societegenerale/aut/test/TestClassWithJunit5Asserts.class";
 
+	private String pathTestClassWithOutJunitAsserts = "./target/aut-target/test-classes/com/societegenerale/aut/test/TestClassWithOutJunitAsserts.class";
 
-    @Test
-    public void shouldThrowForJunit4Violations(){
+	@Before
+	public void setup() {
+		// in the normal lifecycle, ArchUtils is instantiated, which enables a static
+		// field there to be initialized
+		ArchUtils archUtils = new ArchUtils(new SilentLog());
+	}
 
-        Throwable validationExceptionThrown = catchThrowable(() -> {
+	@Test
+	public void shouldNotThrowAnyViolation() {
+		assertThatCode(() -> new NoJunitAssertRuleTest().execute(pathTestClassWithOutJunitAsserts,
+				new DefaultScopePathProvider(), emptySet())).doesNotThrowAnyException();
+	}
 
-            classes().should(NoJunitAssertRuleTest.notUseJunitAssertRule()).check(testClassWithJunit4Asserts);
+	@Test
+	public void shouldThrowForJunit4Violations() {
 
-        });
+		Throwable validationExceptionThrown = catchThrowable(() -> {
 
-        assertThat(validationExceptionThrown).isInstanceOf(AssertionError.class)
-                .hasMessageContaining("was violated (2 times)")
-                .hasMessageContaining("calls method <org.junit.Assert.assertTrue(boolean)")
-                .hasMessageContaining("calls method <org.junit.Assert.fail(java.lang.String)");
+			new NoJunitAssertRuleTest().execute(pathTestClassWithJunit4Asserts, new DefaultScopePathProvider(),
+					emptySet());
 
-        assertThat(validationExceptionThrown).hasMessageStartingWith("Architecture Violation")
-                .hasMessageContaining(TestClassWithJunit4Asserts.class.getName())
-                .hasMessageContaining(NO_JUNIT_ASSERT_DESCRIPTION);
-    }
+		});
 
-    @Test
-    public void shouldThrowForJunit5Violations(){
+		assertThat(validationExceptionThrown).isInstanceOf(AssertionError.class)
+				.hasMessageContaining("was violated (2 times)")
+				.hasMessageContaining("calls method <org.junit.Assert.assertTrue(boolean)")
+				.hasMessageContaining("calls method <org.junit.Assert.fail(java.lang.String)");
 
-        Throwable validationExceptionThrown = catchThrowable(() -> {
+		assertThat(validationExceptionThrown).hasMessageStartingWith("Architecture Violation")
+				.hasMessageContaining(TestClassWithJunit4Asserts.class.getName())
+				.hasMessageContaining(NO_JUNIT_ASSERT_DESCRIPTION);
+	}
 
-            classes().should(NoJunitAssertRuleTest.notUseJunitAssertRule()).check(testClassWithJunit5Asserts);
+	@Test
+	public void shouldThrowForJunit5Violations() {
 
-        });
+		Throwable validationExceptionThrown = catchThrowable(() -> {
 
-        assertThat(validationExceptionThrown).isInstanceOf(AssertionError.class)
-                .hasMessageContaining("was violated (2 times)")
-                .hasMessageContaining("calls method <org.junit.jupiter.api.Assertions.assertEquals(java.lang.Object, java.lang.Object)")
-                .hasMessageContaining("org.junit.jupiter.api.Assertions.fail(java.lang.String)");
+			new NoJunitAssertRuleTest().execute(pathTestClassWithJunit5Asserts, new DefaultScopePathProvider(),
+					emptySet());
 
-        assertThat(validationExceptionThrown).hasMessageStartingWith("Architecture Violation")
-                .hasMessageContaining(TestClassWithJunit5Asserts.class.getName())
-                .hasMessageContaining(NO_JUNIT_ASSERT_DESCRIPTION);
-    }
+		});
+
+		assertThat(validationExceptionThrown).isInstanceOf(AssertionError.class)
+				.hasMessageContaining("was violated (2 times)")
+				.hasMessageContaining(
+						"calls method <org.junit.jupiter.api.Assertions.assertEquals(java.lang.Object, java.lang.Object)")
+				.hasMessageContaining("org.junit.jupiter.api.Assertions.fail(java.lang.String)");
+
+		assertThat(validationExceptionThrown).hasMessageStartingWith("Architecture Violation")
+				.hasMessageContaining(TestClassWithJunit5Asserts.class.getName())
+				.hasMessageContaining(NO_JUNIT_ASSERT_DESCRIPTION);
+	}
 
 }
