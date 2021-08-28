6643d9aabc9dcd4601b60f1e735c2c475e9bc73f
Vincent FUCHS <vincent.fuchs@gmail.com>
2021-08-28 18:44:05 +0200

    introducing scopePaths

--- src/test/java/com/societegenerale/commons/plugin/rules/ArchUtilsTest.java
+++ src/test/java/com/societegenerale/commons/plugin/rules/ArchUtilsTest.java
@@ -9,6 +9,7 @@ import com.tngtech.archunit.core.domain.JavaClass;
 import com.tngtech.archunit.core.domain.JavaClasses;
 import org.junit.Test;
 
+import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
@@ -25,9 +26,9 @@ public class ArchUtilsTest {
 		JavaClasses classes = ArchUtils.importAllClassesInPackage(new RootClassFolder("./target/classes/"),
 				"com/societegenerale/commons/plugin/model");
 
-		long noOfClassesInPackage = classes.stream().count();
+		long noOfClassesInPackage = classes.size();
 
-		assertThat(noOfClassesInPackage).isEqualTo(4);
+		assertThat(noOfClassesInPackage).isEqualTo(5);
 	}
 
 	@Test
@@ -36,7 +37,7 @@ public class ArchUtilsTest {
 
 		long noOfClasses = classes.stream().filter(it -> !it.isNestedClass()).count();
 
-		assertThat(noOfClasses).isEqualTo(36);
+		assertThat(noOfClasses).isEqualTo(37);
 	}
 
 	@Test
@@ -52,7 +53,7 @@ public class ArchUtilsTest {
 				.isNotNull();
 
 		JavaClasses classesWithTestClassesExclusions = ArchUtils.importAllClassesInPackage(new RootClassFolder("./target"), "",
-				Arrays.asList("test-classes"));
+				singletonList("test-classes"));
 
 		assertThat(containsClassWithPattern(classesWithTestClassesExclusions, "ClassToExclude"))
 				.as("when 'test-classes' pattern configured, ClassToExclude should still be found").isTrue();
@@ -74,7 +75,6 @@ public class ArchUtilsTest {
 
 	private boolean containsClassWithPattern(JavaClasses javaClassesToTest, String pattern) {
 
-		return javaClassesToTest.stream().filter(c -> c.getSource().get().getUri().toString().contains(pattern))
-				.findFirst().isPresent();
+		return javaClassesToTest.stream().anyMatch(c -> c.getSource().get().getUri().toString().contains(pattern));
 	}
 }
