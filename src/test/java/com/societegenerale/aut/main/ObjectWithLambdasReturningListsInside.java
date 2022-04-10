3525f58a4896c36aae0c43b75b81daab0f573171
Vincent FUCHS <vincent.fuchs@gmail.com>
2022-04-10 22:47:42 +0200

    minor change in test, to make it more explicit

--- src/test/java/com/societegenerale/aut/main/ObjectWithLambdasReturningListsInside.java
+++ src/test/java/com/societegenerale/aut/main/ObjectWithLambdasReturningListsInside.java
@@ -1,14 +1,16 @@
 package com.societegenerale.aut.main;
 
-import com.google.common.base.Suppliers;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
 
+import javax.annotation.Nonnull;
+
+import com.google.common.base.Suppliers;
+
 public class ObjectWithLambdasReturningListsInside {
 
 	private final Supplier<List<Object>> someCache;
-
 
 	public ObjectWithLambdasReturningListsInside(Supplier<List<Object>> someSupplier) {
 
@@ -18,5 +20,9 @@ public class ObjectWithLambdasReturningListsInside {
 
 	}
 
+	@Nonnull
+	public List<Object> getLIstOfObjects(){
+		return someCache.get();
+	}
 
 }
