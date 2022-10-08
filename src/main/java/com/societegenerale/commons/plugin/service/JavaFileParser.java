79c45676b6c05c2e6a5a7941d3634a223cacb9f2
Vincent Fuchs <vincent.fuchs@gmail.com>
2022-10-08 18:32:40 +0200

    Replacing java parser with Roaster library (#69)
    *  junit 5 upgrade

--- src/main/java/com/societegenerale/commons/plugin/service/JavaFileParser.java
+++ src/main/java/com/societegenerale/commons/plugin/service/JavaFileParser.java
@@ -4,22 +4,12 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-
 import com.societegenerale.commons.plugin.Log;
-import com.tngtech.archunit.thirdparty.com.google.common.annotations.VisibleForTesting;
-import org.apache.commons.lang3.StringUtils;
+import org.jboss.forge.roaster.Roaster;
+import org.jboss.forge.roaster.model.source.JavaSource;
 
 class JavaFileParser
 {
-    protected static final String REGEX_LINE_BREAKS   = "[\n\r]";
-    private static final String PACKAGE             = "package";
-    private static final String CLASS               = "class";
-    private static final String BLOCK_COMMENT_START = "/**";
-    private static final String BLOCK_COMMENT_END   = "*/";
-    private static final String LINE_COMMENT_START  = "//";
-    private static final String LINE_COMMENT_END    = "\n";
 
     /**
      * Parse the file and returns {@link JavaFile}
@@ -27,174 +17,16 @@ class JavaFileParser
      * @param javafilePath - not null
      * @param logger  - not null
      *
-     * @return not null
+     * @return a JavaFile. package attribute may be null if the class is in default package
      */
     JavaFile parse(final Path javafilePath, final Log logger) throws IOException
     {
-        final String fileContent = readFile(javafilePath);
-        String packageString = null;
-        String className = null;
 
-        if (fileContent.isEmpty())
-        {
-            logger.warn("empty file: " + javafilePath);
-        }
-        else
-        {
-            packageString = extractPackage(fileContent, logger);
-            className = extractClassName(fileContent, logger);
-        }
+        var parsedJavaFile=Roaster.parseUnit(Files.newInputStream(javafilePath));
+        JavaSource myClass = parsedJavaFile.getGoverningType();
 
-        return new JavaFile(packageString, className);
-    }
 
-    /**
-     * Extracts the package out of the file content.
-     *
-     * @param javaFileContent - file content as string
-     * @param logger     - not null
-     *
-     * @return null if the package could not be extracted
-     */
-    @VisibleForTesting
-    String extractPackage(final String javaFileContent, @Nonnull final Log logger)
-    {
-        if (StringUtils.isEmpty(javaFileContent) || !javaFileContent.contains(PACKAGE))
-        {
-            return null;
-        }
-
-        final String substring = javaFileContent.substring(javaFileContent.indexOf(PACKAGE));
-
-        // split at the first ";"
-        final String[] split = substring.split(";", 2);
-        // there should be at least 1 part which contains the package declaration
-        if (split.length < 1)
-        {
-            logger.warn("unexpected file content: " + javaFileContent);
-            return null;
-        }
-
-        final String packageDeclaration = split[0].trim();
-        if (!packageDeclaration.startsWith(PACKAGE))
-        {
-            logger.warn("unabble to find package declaration in: " + packageDeclaration);
-            return null;
-        }
-
-        // get the part after the keyword package
-        return packageDeclaration.substring(PACKAGE.length()).trim();
-    }
-
-    /**
-     * Reads the java file.
-     *
-     * @param javafilePath - not null
-     *
-     * @return the file content as String if there is a content, not null
-     *
-     * @throws IOException
-     */
-    @VisibleForTesting
-    String readFile(@Nonnull final Path javafilePath) throws IOException
-    {
-        final StringBuilder builder = new StringBuilder();
-        Files.readAllLines(javafilePath).forEach(builder::append);
-
-        return builder.toString();
-    }
-
-    /**
-     * Extracts the class name.
-     *
-     * @param javaFileContent - file content as String
-     * @param logger     -   not null
-     *
-     * @return class name or null
-     */
-    @VisibleForTesting
-    @Nullable
-    String extractClassName(final String javaFileContent, @Nonnull final Log logger)
-    {
-        if (StringUtils.isEmpty(javaFileContent))
-        {
-            return null;
-        }
-
-        final String commentsRemoved = removeComments(javaFileContent);
-        if (!commentsRemoved.contains(CLASS))
-        {
-            logger.warn("no class found in file with content: " + javaFileContent);
-            return null;
-        }
-
-        final String classOffsetContent = commentsRemoved.substring(commentsRemoved.indexOf(CLASS));
-
-        // split at the first "{"
-        final String[] split = classOffsetContent.split("\\{", 2);
-        // there should be at least 1 part which contains the class declaration
-        if (split.length < 1)
-        {
-            logger.warn("unexpected file content: " + javaFileContent);
-            return null;
-        }
-
-        final String classDeclaration = split[0].trim();
-        if (!classDeclaration.contains(CLASS))
-        {
-            logger.warn("unabble to find class declaration in: " + classDeclaration);
-            return null;
-        }
-
-        // get the part after the keyword class
-        final String substring = classDeclaration.substring(classDeclaration.lastIndexOf(CLASS) + CLASS.length());
-        final String withoutLineBreaks = substring.replaceAll(REGEX_LINE_BREAKS, " ").trim();
-        // remove extends or implements to get only the class name
-        return withoutLineBreaks.split(" ", 2)[0];
-    }
-
-    @VisibleForTesting
-    String removeComments(@Nonnull final String javaFileContent)
-    {
-        final String withOutBlockComments = removeAllBlocks(javaFileContent, BLOCK_COMMENT_START, BLOCK_COMMENT_END);
-        return removeAllBlocks(withOutBlockComments, LINE_COMMENT_START, LINE_COMMENT_END);
-
-    }
-
-    /**
-     * Removes all text blocks defined by blockStartString and blockEndString.
-     *
-     * @param javaFileContent  -   not null
-     * @param blockStartString -   not null
-     * @param blockEndString   -   not null
-     *
-     * @return string without the defined text blocks
-     */
-    private String removeAllBlocks(@Nonnull final String javaFileContent,
-                                   @Nonnull final String blockStartString,
-                                   @Nonnull final String blockEndString)
-    {
-        int startIndex = javaFileContent.indexOf(blockStartString);
-        int endIndex = calEndIndex(blockEndString, startIndex, javaFileContent);
-        String content = javaFileContent;
-
-        while (startIndex >= 0 && endIndex > startIndex)
-        {
-            content = content.substring(0, startIndex) + content.substring(endIndex);
-
-            startIndex = content.indexOf(blockStartString);
-            endIndex = calEndIndex(blockEndString, startIndex, content);
-        }
-
-        return content;
-    }
-
-    private int calEndIndex(@Nonnull final String blockEndString,
-                            final int startIndex,
-                            @Nonnull final String content)
-    {
-        final int endIndex = content.indexOf(blockEndString, startIndex) + blockEndString.length();
-        return endIndex > content.length() ? content.length() : endIndex;
+        return new JavaFile(myClass.getPackage(), myClass.getName());
     }
 
     /**
--- src/test/java/com/societegenerale/commons/plugin/service/JavaFileParserTest.java
+++ src/test/java/com/societegenerale/commons/plugin/service/JavaFileParserTest.java
@@ -5,14 +5,9 @@ import java.io.IOException;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith (MockitoJUnitRunner.class)
 public class JavaFileParserTest extends AbstractExcludePathTest
 {
     private final JavaFileParser javaFileParser = new JavaFileParser();
@@ -30,121 +25,29 @@ public class JavaFileParserTest extends AbstractExcludePathTest
     }
 
     @Test
-    public void testParse() throws IOException
+    public void canParseJavaFile() throws IOException
     {
         final JavaFileParser.JavaFile fileWithPackage = javaFileParser.parse(getTempJavaFile(), getLogger());
-        assertEquals(AbstractExcludePathTest.CLASS_NAME, fileWithPackage.getClassName());
-        assertEquals(AbstractExcludePathTest.PACKAGE_NAME, fileWithPackage.getPackageString());
+        assertThat(fileWithPackage.getClassName()).isEqualTo(AbstractExcludePathTest.CLASS_NAME );
+        assertThat(fileWithPackage.getPackageString()).isEqualTo(AbstractExcludePathTest.PACKAGE_NAME);
+    }
 
+    @Test
+    public void canParseJavaFileWithoutPackage() throws IOException
+    {
         final JavaFileParser.JavaFile fileWithoutPackage = javaFileParser.parse(getTempJavaFileWithDefaultPackage(), getLogger());
-        assertEquals(AbstractExcludePathTest.CLASS_NAME, fileWithoutPackage.getClassName());
-        assertNull(fileWithoutPackage.getPackageString());
+        assertThat(fileWithoutPackage.getClassName()).isEqualTo(AbstractExcludePathTest.CLASS_NAME);
+        assertThat(fileWithoutPackage.getPackageString()).isNull();
+    }
 
+    @Test
+    public void canParseJavaFileWithComments() throws IOException
+    {
         final JavaFileParser.JavaFile fileWithFileComment = javaFileParser.parse(
                 AbstractExcludePathTest.getTempJavaFileWithFileComment(), getLogger());
-        assertEquals(AbstractExcludePathTest.CLASS_NAME_WITH_FILE_COMMENT, fileWithFileComment.getClassName());
-        assertEquals(AbstractExcludePathTest.PACKAGE_NAME, fileWithFileComment.getPackageString());
+        assertThat(fileWithFileComment.getClassName()).isEqualTo(AbstractExcludePathTest.CLASS_NAME_WITH_FILE_COMMENT );
+        assertThat(fileWithFileComment.getPackageString()).isEqualTo(AbstractExcludePathTest.PACKAGE_NAME);
     }
 
-    @Test
-    public void testExtractPackage_FileContentNull()
-    {
-        javaFileParser.extractPackage(null, getLogger());
-    }
 
-    @Test
-    public void testExtractPackage_FileContentEmpty()
-    {
-        javaFileParser.extractPackage("", getLogger());
-    }
-
-    @Test
-    public void testExtractPackage_DefaultPackage()
-    {
-        final String defaultPackage = javaFileParser.extractPackage(
-                AbstractExcludePathTest.CONTENT_WITH_DEFAULT_PACKAGE, getLogger());
-        assertNull(defaultPackage);
-    }
-
-    @Test
-    public void testExtractPackage()
-    {
-        final String extractPackage = javaFileParser.extractPackage(AbstractExcludePathTest.CONTENT_WITH_PACKAGE,
-                                                                    getLogger());
-        assertEquals(AbstractExcludePathTest.PACKAGE_NAME, extractPackage);
-
-        final String defaultPackage = javaFileParser.extractPackage(
-                AbstractExcludePathTest.CONTENT_WITH_DEFAULT_PACKAGE, getLogger());
-        assertNull(defaultPackage);
-    }
-
-    @Test
-    public void testReadFile() throws IOException
-    {
-        final String fileContentDefaultPackage = javaFileParser.readFile(getTempJavaFileWithDefaultPackage());
-        assertEquals(removeLineBreaks(AbstractExcludePathTest.CONTENT_WITH_DEFAULT_PACKAGE), fileContentDefaultPackage);
-
-        final String fileContent = javaFileParser.readFile(getTempJavaFile());
-        assertEquals(removeLineBreaks(AbstractExcludePathTest.CONTENT_WITH_PACKAGE), fileContent);
-
-        final String fileContentWithFileComment = javaFileParser.readFile(
-                AbstractExcludePathTest.getTempJavaFileWithFileComment());
-        assertEquals(removeLineBreaks(AbstractExcludePathTest.CONTENT_WITH_FILE_COMMENT_AND_PACKAGE),
-                     fileContentWithFileComment);
-    }
-
-    @Test
-    public void testExtractClassName()
-    {
-        final String className1 = javaFileParser.extractClassName(AbstractExcludePathTest.CONTENT_WITH_PACKAGE,
-                                                                  getLogger());
-        assertEquals(AbstractExcludePathTest.CLASS_NAME, className1);
-
-        final String className2 = javaFileParser.extractClassName(AbstractExcludePathTest.CONTENT_WITH_DEFAULT_PACKAGE,
-                                                                  getLogger());
-        assertEquals(AbstractExcludePathTest.CLASS_NAME, className2);
-
-        final String className3 = javaFileParser.extractClassName(
-                AbstractExcludePathTest.CONTENT_WITH_FILE_COMMENT_AND_PACKAGE,
-                getLogger());
-        assertEquals(AbstractExcludePathTest.CLASS_NAME_WITH_FILE_COMMENT, className3);
-    }
-
-    @Test
-    public void testExtractClassNameClassInComments()
-    {
-        final String className = javaFileParser.extractClassName(
-                AbstractExcludePathTest.CONTENT_WITH_CLASS_IN_COMMENT_AND_PACKAGE,
-                getLogger());
-        assertNull(className);
-    }
-
-    @Test
-    public void testRemoveComments()
-    {
-        final String blockComment = "/**\n\r"
-                                    + "* block comment for class foo\n\r"
-                                    + " */";
-
-        final String onelineBlockcomment = "/** one line block comment for class foo */";
-        final String lineComment = "// line comment for class foo \n";
-
-        final String expected = "bar";
-
-        assertTrue(javaFileParser.removeComments(lineComment).isEmpty());
-        assertEquals(expected, javaFileParser.removeComments(lineComment + expected));
-        assertEquals(expected, javaFileParser.removeComments(lineComment + expected + lineComment));
-
-        assertEquals(expected, javaFileParser.removeComments(onelineBlockcomment + expected));
-        assertEquals(expected, javaFileParser.removeComments(onelineBlockcomment + expected + onelineBlockcomment));
-
-        assertEquals(expected, javaFileParser.removeComments(blockComment + expected));
-        assertEquals(expected, javaFileParser.removeComments(blockComment + expected + blockComment));
-
-    }
-
-    private String removeLineBreaks(final String s)
-    {
-        return s.replaceAll(JavaFileParser.REGEX_LINE_BREAKS, "");
-    }
 }
