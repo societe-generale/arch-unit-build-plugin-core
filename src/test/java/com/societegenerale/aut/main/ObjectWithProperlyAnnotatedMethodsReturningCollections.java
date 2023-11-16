1a1627e51ae625ef39d7adc5abcaaa25cae04c9e
Paul Williams <mailme@paulwilliams.in>
2023-11-16 09:33:04 -0500

    fix: jakarta Nonnull assertion for method returning collections (#75)
    (cherry picked from commit e5f6097dd7d3d173dc891954e0f597d486ee8372)

--- src/test/java/com/societegenerale/aut/main/ObjectWithProperlyAnnotatedMethodsReturningCollections.java
+++ src/test/java/com/societegenerale/aut/main/ObjectWithProperlyAnnotatedMethodsReturningCollections.java
@@ -1,6 +1,5 @@
 package com.societegenerale.aut.main;
 
-import javax.annotation.Nonnull;
 import java.util.List;
 import java.util.Set;
 
@@ -9,14 +8,23 @@ import static java.util.Collections.emptySet;
 
 public class ObjectWithProperlyAnnotatedMethodsReturningCollections {
 
-	@Nonnull
-	public List returningANullList(){
+	@jakarta.annotation.Nonnull
+	public List returningANullListWithJakartaNonnullAnnotation(){
 		return emptyList();
 	}
 
-	@Nonnull
-	public Set returningANullSet(){
+	@jakarta.annotation.Nonnull
+	public Set returningANullSetWithJakartaNonnullAnnotation(){
 		return emptySet();
 	}
 
+	@javax.annotation.Nonnull
+	public List returningANullListWithJavaxNonnullAnnotation(){
+		return emptyList();
+	}
+
+	@javax.annotation.Nonnull
+	public Set returningANullSetWithJavaxNonnullAnnotation(){
+		return emptySet();
+	}
 }
